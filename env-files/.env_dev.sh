02833c8d098ffa29fe59f82325d09a6f72180897
araalifarooq <farooqp35h@gmail.com>
2018-11-02 23:35:59 +0300

    Adding role to user response

---
+++ env-files/.env_dev.sh
@@ -0,0 +1,6 @@
+export APP_SETTINGS="DEVELOPMENT"
+export DATABASE_NAME="store_manager"
+export DATABASE_USER="araalifarooq"
+export DATABASE_PASSWORD="araali"
+export DATABASE_PORT=5432
+export DATABASE_HOST="localhost" 
---
+++ env-files/.env_test.sh
@@ -0,0 +1,6 @@
+export APP_SETTINGS="TESTING"
+export DATABASE_NAME="store_manager_testing"
+export DATABASE_USER="araalifarooq"
+export DATABASE_PASSWORD="araali"
+export DATABASE_PORT=5432
+export DATABASE_HOST="localhost" 
